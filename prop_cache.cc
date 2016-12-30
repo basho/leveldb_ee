ba53a72d1d8199ea14b1398790dac58ddac63d23
Matthew V <matthewv@basho.com>
2016-12-29 20:36:04 -0500

    fill out initial code for Lookup and Insert

--- prop_cache.cc
+++ prop_cache.cc
@@ -22,8 +22,9 @@
 
 
 #include "leveldb_ee/prop_cache.h"
+#include "leveldb_ee/riak_object.h"
 #include "util/logging.h"
-
+#include "util/mutexlock.h"
 
 namespace leveldb {
 
@@ -36,9 +37,10 @@ static PropertyCache * lPropCache(NULL);
  * leveldb initialization
  */
 void
-PropertyCache::InitPropertyCache()
+PropertyCache::InitPropertyCache(
+    EleveldbRouter_t Router)
 {
-    lPropCache = new PropertyCache;
+    lPropCache = new PropertyCache(Router);
 
     return;
 
@@ -67,8 +69,10 @@ PropertyCache::GetCache()
 /**
  * Construct property cache object (likely singleton)
  */
-PropertyCache::PropertyCache()
-    : m_Cache(NULL)
+PropertyCache::PropertyCache(
+    EleveldbRouter_t Router)
+    : m_Cache(NULL), m_Router(Router),
+      m_Cond(&m_Mutex)
 {
     // 1000 is number of cache entries.  Just pulled
     //  that number out of the air.
@@ -82,6 +86,143 @@ PropertyCache::~PropertyCache()
     delete m_Cache;
     m_Cache=NULL;
 }   // PropertyCache::~PropertyCache
+
+
+/**
+ * Retrieve property from cache if available,
+ *  else call out to Riak to get properties
+ */
+Cache::Handle *
+PropertyCache::Lookup(
+    const Slice & CompositeBucket)
+{
+    Cache::Handle * ret_handle(NULL);
+    assert(NULL!=lPropCache);
+    assert(NULL!=lPropCache->m_Cache);
+
+    ret_handle=GetCache().Lookup(CompositeBucket);
+
+    // not waiting in the cache already.  Request info
+    if (NULL==ret_handle && NULL!=lPropCache->m_Router)
+    {
+        ret_handle=lPropCache->LookupWait(CompositeBucket);
+    }   // if
+
+    return(ret_handle);
+
+}   // PropertyCache::Lookup
+
+
+
+static void
+DeleteProperty(
+    const Slice& key,
+    void* value)
+{
+    ExpiryModuleOS * expiry;
+
+    expiry=(ExpiryModuleOS *)value;
+
+    delete expiry;
+}   // static DeleteProperty
+
+
+/**
+ * (static) Add / Overwrite key in property cache.  Manage handle
+ *  on caller's behalf
+ */
+bool
+PropertyCache::Insert(
+    const Slice & CompositeBucket,
+    void * Props,
+    Cache::Handle ** OutputPtr)
+{
+    assert(NULL!=lPropCache);
+    bool ret_flag(false);
+    Cache::Handle * ret_handle(NULL);
+
+    ret_handle=lPropCache->InsertInternal(CompositeBucket, Props);
+
+    if (NULL!=OutputPtr)
+        *OutputPtr=ret_handle;
+    else if (NULL!=ret_handle)
+        GetCache().Release(ret_handle);
+
+    ret_flag=(NULL!=ret_handle);
+
+    return(ret_flag);
+
+}   // PropertyCache::Insert
+
+
+Cache::Handle *
+PropertyCache::InsertInternal(
+    const Slice & CompositeBucket,
+    void * Props)
+{
+    assert(NULL!=m_Cache);
+
+    Cache::Handle * ret_handle(NULL);
+
+    {
+        MutexLock lock(&m_Mutex);
+
+        ret_handle=m_Cache->Insert(CompositeBucket, Props, 1, DeleteProperty);
+        m_Cond.SignalAll();
+    }
+
+    return(ret_handle);
+
+}   // PropertyCache::InsertInternal
+
+
+/**
+ * Internal Lookup function that first requests property
+ *  data from Eleveldb Router, then waits for the data
+ *  to post to the cache.
+ */
+Cache::Handle *
+PropertyCache::LookupWait(
+    const Slice & CompositeBucket)
+{
+    Cache::Handle * ret_handle(NULL);
+    std::string type, bucket;
+    const void * params[4];
+    bool flag;
+
+    // split composite to pass to Riak
+    KeyParseBucket(CompositeBucket, type, bucket);
+
+    params[0]=type.c_str();
+    params[1]=bucket.c_str();
+    params[2]=(void *)&CompositeBucket;
+    params[3]=NULL;
+    flag=m_Router(eGetBucketProperties, 3, params);
+
+    // proceed with wait loop if router call successfull
+    if (flag)
+    {
+        do
+        {
+            // has value populated since last look?
+            ret_handle=m_Cache->Lookup(CompositeBucket);
+
+            // is state appropriate to waiting?
+            if (NULL==ret_handle && flag)
+            {
+                timespec ts;
+                MutexLock lock(&m_Mutex);
+
+                clock_gettime(CLOCK_REALTIME, &ts);
+                ts.tv_sec+=1;
+                flag=m_Cond.Wait(&ts);
+            }   // if
+        } while(NULL==ret_handle && flag);
+    }   // if
+
+    return(ret_handle);
+
+}   // PropertyCache::LookupWait
 
 }  // namespace leveldb
 
--- prop_cache.h
+++ prop_cache.h
@@ -25,6 +25,8 @@
 
 #include "leveldb/cache.h"
 #include "leveldb_ee/expiry_ee.h"
+#include "port/port.h"
+
 
 namespace leveldb
 {
@@ -36,31 +38,131 @@ class PropertyCache
 {
 public:
     // create global cache object
-    static void InitPropertyCache();
+    static void InitPropertyCache(EleveldbRouter_t Router);
 
     // release global cache object
     static void ShutdownPropertyCache();
 
-    // static entry point
-    static bool GetExpiryProperties(const Slice & CompositeBucket,
-                                    ExpiryPropPtr_t & PropOut) {return(false);};
+    // static lookup, usually from CachePtr
+    static Cache::Handle * Lookup(const Slice & CompositeBucket);
 
-    // static Accessor for populating smart cache pointers
+    // static insert, usually from eleveldb::property_cache()
+    static bool Insert(const Slice & CompositeBucket, void * Props, Cache::Handle ** OutputPtr);
+
+    // static retrieval of active cache
     static Cache & GetCache();
-
-    PropertyCache();
 
     // virtual destructor to facilitate unit tests
     virtual ~PropertyCache();
 
 protected:
+    // only allow creation from InitPropertyCache();
+    PropertyCache(EleveldbRouter_t);
+
+    // accessor to m_Cache pointer (really bad if NULL m_Cache)
     Cache * GetCachePtr() {return(m_Cache);};
 
+    // internal routine to launch lookup request via eleveldb router, then wait
+    Cache::Handle * LookupWait(const Slice & CompositeBucket);
+
+    // internal routine to insert object and signal condition variable
+    Cache::Handle * InsertInternal(const Slice & CompositeBucket, void * Props);
+
+
     Cache * m_Cache;
+    EleveldbRouter_t m_Router;
+    port::Mutex m_Mutex;
+    port::CondVar m_Cond;
+
+private:
+    PropertyCache();
+    PropertyCache(const PropertyCache &);
+    PropertyCache operator=(const PropertyCache &);
+
+}; // class PropertyCache
+
+
+/**
+ * This template wraps an object in property cache
+ *  to insure it is properly released.
+ *  Makes calls to static functions of PropertyCache.
+ */
+template<typename Object> class CachePtr
+{
+    /****************************************************************
+    *  Member objects
+    ****************************************************************/
+public:
+
+protected:
+    Object * m_Ptr;            // NULL or object in cache
 
 private:
 
-}; // class PropertyCache
+    /****************************************************************
+    *  Member functions
+    ****************************************************************/
+public:
+    CachePtr() : m_Ptr(NULL) {};
+
+    ~CachePtr() {Release();};
+
+    void Release()
+    {
+        if (NULL!=m_Ptr)
+            PropertyCache::GetCache().Release((Cache::Handle*)m_Ptr);
+        m_Ptr=NULL;
+    };
+
+    CachePtr & operator=(Cache::Handle * Hand) {reset(Hand);};
+
+    void assign(Object * Ptr) {reset(Ptr);};
+
+    void reset(Object * ObjectPtr=NULL)
+    {
+        Release();
+        m_Ptr=(Object *)ObjectPtr;
+    }
+
+    Object * get() {return(m_Ptr);};
+
+    const Object * get() const {return(m_Ptr);};
+
+    Object * operator->() {return(m_Ptr);};
+    const Object * operator->() const {return(m_Ptr);};
+
+    Object & operator*() {return(*get());};
+    const Object & operator*() const {return(*get());};
+
+    bool Lookup(const Slice & Key)
+    {
+        Release();
+        m_Ptr=(Object *)PropertyCache::Lookup(Key);
+        return(NULL!=m_Ptr);
+    };
+
+    bool Insert(const Slice & Key, Object * Value)
+    {
+        Release();
+        m_Ptr=(Object *)PropertyCache::GetCache().Insert(Key, Value, 1, &Deleter);
+        return(NULL!=m_Ptr);
+    };
+
+    static void Deleter(const Slice& Key, void * Value)
+    {
+        Object * ptr(Value);
+        delete ptr;
+    };
+protected:
+
+private:
+    CachePtr(const CachePtr &);
+    CachePtr & operator=(const CachePtr &);
+
+};  // template CachePtr
+
+
+typedef CachePtr<ExpiryProperties> ExpiryPropPtr_t;
 
 
 }  // namespace leveldb
